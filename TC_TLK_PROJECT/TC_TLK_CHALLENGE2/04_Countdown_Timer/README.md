13fdf4a38abb1a643cfda6a5d2e8613c90fd47fd
1096618 <1096618@pdsb.net>
2025-03-18 09:03:39 -0400

    Initial Commit

---
+++ TC_TLK_PROJECT/TC_TLK_CHALLENGE2/04_Countdown_Timer/README.md
@@ -0,0 +1,6 @@
+PROBLEM: Write a program that starts at `10` and counts down to `1`, but if the user enters `"stop"`, the countdown should break.  
+
+Pseudo code:
+1. Input the amount of tick to countdown from
+2. Loop until countdown reach 0
+3. If user type `stop` in between each tick then loop will stop
--- TC_TLK_PROJECT/TC_TLK_CHALLENGE2/04_Countdown_Timer/TC_04_Countdown_Timer.py
+++ TC_TLK_PROJECT/TC_TLK_CHALLENGE2/04_Countdown_Timer/TC_04_Countdown_Timer.py
@@ -1,11 +1,26 @@
-import time
-countdown = int(input("How many second to countdown to? "))
+# -----------------------------------------------------------------------------
+# Name:        Countdown timer
+# Purpose:     Write a program that starts at `10` and counts down to `1`, but if the user enters `"stop"`, the countdown should break.
+#
+# Author:      TC
+# Created:     March 18, 2025
+# -----------------------------------------------------------------------------
+
+#Note: i wanted to make an actual countdown timer and print with a delay of 1 second and can stop whenever you type stop while still counting down
+#without input but it impossible without threading or async or something which are too advanced it seem
+
+#Input in how many tick to countdown from
+countdown = int(input("How many tick to countdown to? "))
+
+#Loop until countdown is 0
 while countdown > 0:
+    print(countdown)
+    countdown -= 1
+
+    #If user type "stop", the loop will stop
     stop = input()
     if stop == "stop":
         break
-    print(countdown)
-    countdown -= 1
-    time.sleep(1)
 
+print("Countdown timer stopped")
 

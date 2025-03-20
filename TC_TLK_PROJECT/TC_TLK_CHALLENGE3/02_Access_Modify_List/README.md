8ee28355e5faae6e8e3a78d937b1f9de525cd2d8
1096618 <1096618@pdsb.net>
2025-03-20 08:33:11 -0400

    Initial Commit

---
+++ TC_TLK_PROJECT/TC_TLK_CHALLENGE3/02_Access_Modify_List/README.md
@@ -0,0 +1,8 @@
+Problem: Create a grocery list and modify existing item in the list
+
+Pseudo code:
+1. Create a grocery list with pre-existing item in it
+2. Ask the user what item in the list should be replace
+3. If item exist in the list ask user what should it be repalce with
+4. Get the index value of the item should be replace with and replace it with new item
+5. Display new list
--- TC_TLK_PROJECT/TC_TLK_CHALLENGE3/02_Access_Modify_List/TC_02_Access_Modify_List.py
+++ TC_TLK_PROJECT/TC_TLK_CHALLENGE3/02_Access_Modify_List/TC_02_Access_Modify_List.py
@@ -1,15 +1,23 @@
 # -----------------------------------------------------------------------------
 # Name:        Grocery List - Access and modify
-# Purpose:     Create a list of groceries and then add new items to the list.
+# Purpose:     Modify the list by changing existing item in the list.
 #
 # Author:      TC
 # Created:     March 19, 2025
 # -----------------------------------------------------------------------------
 
+#Create a list with all the grocery items and display them
 grocerylist = ["pen", "pineapple", "apple", "ballpoint pen"]
 print("The current grocery list is:", ", ".join(grocerylist))
+
+#Ask the user which item in the list to be replace
 litem = input("What item in the list would you like to change? ")
 if litem in grocerylist:
+
+    #If item does exist then ask what item does user want to replace with
     nitem = input("What item would you like to replace it with ")
     grocerylist[grocerylist.index(litem)] = nitem
     print("The current grocery list is:", ", ".join(grocerylist))
+
+else:
+    print("Sorry, the item you are looking for is not in your grocery list.")

41a74ecc22bdd4e5a13290355b86993d7af8a99c
1096618 <1096618@pdsb.net>
2025-05-20 09:42:44 -0400

    Initial Commit

--- TC_Password_Manager/TC_Password_Manager.py
+++ TC_Password_Manager/TC_Password_Manager.py
@@ -1,3 +1,4 @@
+import tkinter as tk
 import customtkinter
 
 customtkinter.set_appearance_mode("dark")
@@ -21,28 +22,39 @@ class PasswordManagerApp(customtkinter.CTk):
 
         # Login label
         login_label = customtkinter.CTkLabel(self, text="Login", font=("Comic Sans MS", 30), text_color="white")
-        login_label.pack(padx=0, pady=46, anchor="center")
+        login_label.pack(padx=0, pady=45, anchor="center")
 
         # Username input
-        #self.username_input = customtkinter.CTkTextbox(self, width=250, height=50)
-        #self.username_input.place(relx=0.5, rely=0.47, anchor="center")
+        self.username_entry = customtkinter.CTkEntry(self, placeholder_text="Username", font=("Comic Sans MS", 15)
+                                                     , width=250, height=50)
+        self.username_entry.place(relx=0.5, rely=0.45, anchor="center")
 
-        username_label = customtkinter.CTkLabel(self, text="", font=("Comic Sans MS", 30),)
-        username_label.pack(pady=46)
+        # Password input
+        self.password_entry = customtkinter.CTkEntry(self, placeholder_text="Password", font=("Comic Sans MS", 15)
+                                                     , width=250, height=50)
+        self.password_entry.place(relx=0.5, rely=0.56, anchor="center")
 
-        username_entry = customtkinter.CTkEntry(self, placeholder_text="Username")
-        username_entry.place(relx=0.5, rely=0.47, anchor="center")
+        #Create new account
+        self.createaccount_label = customtkinter.CTkLabel(self, text="Create Account"
+                                                          , font=("Comic Sans MS", 15, "underline")
+                                                          , fg_color="transparent")
+        self.createaccount_label.configure(cursor="hand2")
+        self.createaccount_label.bind("<Button-1>", self.create_account_clicked)
+        self.createaccount_label.place(relx=0.335, rely=0.76, anchor="center")
 
         # Login button
         login_button = customtkinter.CTkButton(self, text="Login", font=("Comic Sans MS", 25), width=250, height=50,
                                                command=self.login_button_pressed)
-        login_button.place(relx=0.5, rely=0.8, anchor="center")
+        login_button.place(relx=0.5, rely=0.67, anchor="center")
+
+    def create_account_clicked(self, event):
+        print("create new account clicked")
 
     def login_button_pressed(self):
-        # Handle the login button press
-        print(f"Username entered: {self.username_input.get('1.0', 'end-1c')}")
-        # Add login logic here
-
+        username = self.username_entry.get()
+        password = self.password_entry.get()
+        print(f"Username entered: {username}")
+        print(f"Password entered: {password}")
 
 # Run the app
 app = PasswordManagerApp()

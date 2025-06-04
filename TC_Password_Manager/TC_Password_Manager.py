9a3c17e9a326a5e3bf7b4a3b8401da83d9c68fa4
1096618 <1096618@pdsb.net>
2025-06-04 09:42:05 -0400

    Initial Commit

--- TC_Password_Manager/TC_Password_Manager.py
+++ TC_Password_Manager/TC_Password_Manager.py
@@ -1,15 +1,25 @@
 import hashlib
 
 import customtkinter
+import tkinter
+from tkinter import font
 import sqlite3
 import bcrypt
+import atexit
 from cryptography.fernet import Fernet
+import time
 
 #import pyinstaller // download later
 
 
 connection = sqlite3.connect('TC_Password_Manager.db')
 cursor = connection.cursor()
+
+
+# close db when you press x icon
+def cleanup():
+    connection.commit()
+    connection.close()
 
 def create_tables():
     # Connect to the SQLite database (this will create the database if it doesn't exist)
@@ -56,7 +66,8 @@ class PasswordManagerApp(customtkinter.CTk):
         self.resizable(False, False)
 
         # Call the function to set up the login page
-        self.login_page()
+        #self.login_page()
+        self.main_menu()
 
     def appearance_theme(self):
         customtkinter.set_appearance_mode("dark")
@@ -64,34 +75,34 @@ class PasswordManagerApp(customtkinter.CTk):
 
     def login_page(self):
         # Title
-        title_label = customtkinter.CTkLabel(self, text="TC Password Manager", font=("Comic Sans MS", 32),
+        title_label = customtkinter.CTkLabel(self, text="TC Password Manager", font=("Consolas", 32),
                                              text_color="white")
         title_label.pack(padx=0, pady=50, anchor="center")
 
         # Login label
-        login_label = customtkinter.CTkLabel(self, text="Login", font=("Comic Sans MS", 30), text_color="white")
+        login_label = customtkinter.CTkLabel(self, text="Login", font=("Consolas", 30), text_color="white")
         login_label.pack(padx=0, pady=45, anchor="center")
 
         # Username input
-        self.username_entry = customtkinter.CTkEntry(self, placeholder_text="Username", font=("Comic Sans MS", 15)
+        self.username_entry = customtkinter.CTkEntry(self, placeholder_text="Username", font=("Consolas", 15)
                                                      , width=250, height=50)
         self.username_entry.place(relx=0.5, rely=0.45, anchor="center")
 
         # Password input
-        self.password_entry = customtkinter.CTkEntry(self, placeholder_text="Password", font=("Comic Sans MS", 15)
-                                                     , width=250, height=50)
+        self.password_entry = customtkinter.CTkEntry(self, placeholder_text="Password", font=("Consolas", 15)
+                                                     , width=250, height=50) #put show="*" later
         self.password_entry.place(relx=0.5, rely=0.56, anchor="center")
 
         #Create new account
         self.createaccount_label = customtkinter.CTkLabel(self, text="Create Account"
-                                                          , font=("Comic Sans MS", 15, "underline")
+                                                          , font=("Consolas", 15, "underline")
                                                           , fg_color="transparent")
         self.createaccount_label.configure(cursor="hand2")
         self.createaccount_label.bind("<Button-1>", self.create_account_clicked)
         self.createaccount_label.place(relx=0.345, rely=0.76, anchor="center")
 
         # Login button
-        login_button = customtkinter.CTkButton(self, text="Login", font=("Comic Sans MS", 25), width=250, height=50,
+        login_button = customtkinter.CTkButton(self, text="Login", font=("Consolas", 25), width=250, height=50,
                                                command=self.login_button_pressed)
         login_button.place(relx=0.5, rely=0.67, anchor="center")
 
@@ -106,6 +117,7 @@ class PasswordManagerApp(customtkinter.CTk):
         if hasattr(self, "green_frame") and self.green_frame.winfo_exists():
             self.green_frame.destroy()
 
+        # CHECK IF INPUTTED INFO IS A VALID INPUT
         if not login_username or not login_password:
             self.show_error("Please enter all fields", width=160, relx=0.5, rely=0.94, anchor="center")
         elif len(login_username) < 3:
@@ -114,9 +126,15 @@ class PasswordManagerApp(customtkinter.CTk):
             cursor.execute("SELECT password_hash FROM users WHERE username = ?", (login_username,))
             row = cursor.fetchone()
 
+            # IF VALID IN PUT THEN BLABLA
             if row and bcrypt.checkpw(login_password.encode(), row[0].encode()):
                 print("Login successful")
                 self.correct_frame("Login Successful", width=160, relx=0.5, rely=0.94, anchor="center")
+
+                self.clear_screen()
+                time.sleep(3)
+                self.main_menu()
+
             else:
                 self.show_error("Invalid password or username", width=208, relx=0.5
                                                                        , rely=0.94, anchor="center")
@@ -134,12 +152,12 @@ class PasswordManagerApp(customtkinter.CTk):
 
     def create_account_page(self):
         # Create Title
-        create_title_label = customtkinter.CTkLabel(self, text="TC Password Manager", font=("Comic Sans MS", 32),
+        create_title_label = customtkinter.CTkLabel(self, text="TC Password Manager", font=("Consolas", 32),
                                              text_color="white")
         create_title_label.pack(padx=0, pady=50, anchor="center")
 
         # Create account label
-        create_account_label = customtkinter.CTkLabel(self, text="Create account", font=("Comic Sans MS", 30)
+        create_account_label = customtkinter.CTkLabel(self, text="Create account", font=("Consolas", 30)
                                              , text_color="white")
         create_account_label.pack(padx=0, pady=45, anchor="center")
 
@@ -147,7 +165,7 @@ class PasswordManagerApp(customtkinter.CTk):
         self.create_username_entry = customtkinter.CTkEntry(
             self,
             placeholder_text="Username",
-            font=("Comic Sans MS", 15),
+            font=("Consolas", 15),
             width=250,
             height=50,
             fg_color="#2b2b2b",
@@ -160,7 +178,7 @@ class PasswordManagerApp(customtkinter.CTk):
         self.create_password_entry = customtkinter.CTkEntry(
             self,
             placeholder_text="Password",
-            font=("Comic Sans MS", 15),
+            font=("Consolas", 15),
             width=250,
             height=50,
             fg_color="#2b2b2b",
@@ -173,7 +191,7 @@ class PasswordManagerApp(customtkinter.CTk):
         self.confirm_password_entry = customtkinter.CTkEntry(
             self,
             placeholder_text="Confirm password",
-            font=("Comic Sans MS", 15),
+            font=("Consolas", 15),
             width=250,
             height=50,
             fg_color="#2b2b2b",
@@ -190,7 +208,7 @@ class PasswordManagerApp(customtkinter.CTk):
         self.focus()    # Return focus to the root window
 
         # Create account button
-        create_account_button = customtkinter.CTkButton(self, text="Create account", font=("Comic Sans MS", 25)
+        create_account_button = customtkinter.CTkButton(self, text="Create account", font=("Consolas", 25)
                                                         , width=250
                                                         , height=50
                                                         ,command=self.create_account_button_pressed)
@@ -199,10 +217,10 @@ class PasswordManagerApp(customtkinter.CTk):
         #Switch back from create to login page
         switchx = 0.45
         switch_logintext_label = customtkinter.CTkLabel(self, text="Already have an account?"
-                                                        , font=("Comic Sans MS", 15))
+                                                        , font=("Consolas", 15))
         switch_logintext_label.place(relx=switchx, rely=0.87, anchor="center")
         self.switch_login_label = customtkinter.CTkLabel(self, text="Log in"
-                                                          , font=("Comic Sans MS", 15, "underline")
+                                                          , font=("Consolas", 15, "underline")
                                                           , fg_color="transparent")
         self.switch_login_label.configure(cursor="hand2")
         self.switch_login_label.bind("<Button-1>", self.switch_login_clicked)
@@ -223,7 +241,7 @@ class PasswordManagerApp(customtkinter.CTk):
 
         self.error_label = customtkinter.CTkLabel(master=self.red_frame,
                                                   text=message,
-                                                  font=("Comic Sans MS", 15),
+                                                  font=("Consolas", 15),
                                                   bg_color="transparent")
         self.error_label.place(relx=0.5, rely=0.5, anchor="center")
 
@@ -237,7 +255,7 @@ class PasswordManagerApp(customtkinter.CTk):
 
         self.success_label = customtkinter.CTkLabel(master=self.green_frame,
                                                     text=message,
-                                                    font=("Comic Sans MS", 15),
+                                                    font=("Consolas", 15),
                                                     bg_color="transparent")
         self.success_label.place(relx=0.5, rely=0.5, anchor="center")
 
@@ -272,23 +290,64 @@ class PasswordManagerApp(customtkinter.CTk):
 
             #     INPUTTING USER INFO INTO DATABASE
             try:
-                cursor.execute("""
-                    INSERT INTO users (username, password, password_hash) 
-                    VALUES (?, ?, ?);
-                """, (new_username,plain_password, hashed_password_str))
-                connection.commit()
-                print("User registered successfully.")
-                self.correct_frame("User registered successfully.", width=208, relx=0.5
-                                    , rely=0.94, anchor="center")
-
+                with sqlite3.connect("TC_Password_Manager.db") as conn:
+                    cursor = conn.cursor()
+                    cursor.execute("""
+                        INSERT INTO users (username, password, password_hash) 
+                        VALUES (?, ?, ?);
+                    """, (new_username, plain_password, hashed_password_str))
+                    conn.commit()
+                    print("User registered successfully.")
+                    self.correct_frame("User registered successfully.", width=208, relx=0.5, rely=0.94
+                                       , anchor="center")
             except sqlite3.IntegrityError as e:
                 print(f"Error: {e}")
-            finally:
-                connection.close()
+
+    def main_menu(self):
+        print("main menu")
+        #top frame
+        self.top_bar()
+
+    def top_bar(self):
+        self.top_frame = customtkinter.CTkFrame(self, width=480, height=52, fg_color="#2c2c2c", corner_radius=0)
+        self.top_frame.place(relx=0.5, rely=0, anchor="n")
+        
+        #text shennanigan
+        self.text_widget = tkinter.Text(self.top_frame,
+                                        font=("Consolas", 20),
+                                        height=2,  # height of widget box
+                                        width=40,  # width of widget box
+                                        bd=0,  # border = 0 aka off
+                                        highlightthickness=0,  # remove highlight border
+                                        bg="#2b2b2b",  # fixed dark background
+                                        fg="white",  # fixed text color
+                                        insertbackground="white",  # fixed cursor color
+                                        selectbackground="#555555"  # fixed selection background
+                                        )
+        self.text_widget.place(relx=0.5, anchor="n")
+        self.text_widget.configure(state="disabled")
+
+        #variable for animation typing
+        self.full_text = ""
+        self.keyword = ""
+        self.keycolor =""
+        self.keyword_styles =[]
+        self.current_index = 0
+
+        #self.typing_text(text,keyword,keycolor,keyword_styles=[])
+
+    def typing_text(self,text, keyword, keycolor, keyword_styles):
+        self.full_text = text
+        self.keyword = keyword
+        self.keycolor = keycolor
+        self.keyword_styles = keyword_styles or []
+        self.current_index = 0
+        
 
 
 
 # Run the app
 create_tables()
+atexit.register(cleanup)
 app = PasswordManagerApp()
 app.mainloop()
